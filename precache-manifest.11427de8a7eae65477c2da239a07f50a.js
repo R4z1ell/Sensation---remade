ee9c6d5049e2cedf7b77815f1b87261c231f311f
R4z1ell <creawshaka@gmail.com>
2019-04-15 15:37:47 +0200

    Updates

---
+++ precache-manifest.11427de8a7eae65477c2da239a07f50a.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "ec7e6aeacde48c39a98a",
+    "url": "/Sensation---remade/static/js/main.1e49e5fc.chunk.js"
+  },
+  {
+    "revision": "27fd582200f667224fbc",
+    "url": "/Sensation---remade/static/js/runtime~main.03373b7c.js"
+  },
+  {
+    "revision": "3f24e91ee8d7714b3f2f",
+    "url": "/Sensation---remade/static/js/2.780b8572.chunk.js"
+  },
+  {
+    "revision": "28f02ca7dc9c31c467ac12da6728442c",
+    "url": "/Sensation---remade/index.html"
+  }
+];
